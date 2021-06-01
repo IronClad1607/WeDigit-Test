5be9770dc246d21f77b06840bcc0baeeb17bdb38
Ishaan Jain <ishaan160799@gmail.com>
2021-06-01 21:15:05 +0530

    nav added

--- app/src/main/java/com/ironclad/wedigittest/view/fragments/SplashFragment.kt
+++ app/src/main/java/com/ironclad/wedigittest/view/fragments/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.ironclad.wedigittest.view.fragments
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ironclad.wedigittest.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
@@ -20,8 +23,19 @@ class SplashFragment : Fragment() {
         return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(SplashFragmentDirections.goToFeed())
+        }, SPLASH_DELAY)
+    }
+
     override fun onDestroy() {
         binding = null
         super.onDestroy()
+    }
+
+    companion object {
+        const val SPLASH_DELAY: Long = 3000
     }
 }
