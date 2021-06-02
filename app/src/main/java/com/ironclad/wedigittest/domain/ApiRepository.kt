d0acf562924335cc59abb2a2b8b42089e2c61375
Ishaan Jain <ishaan160799@gmail.com>
2021-06-02 14:26:50 +0530

    repo and build config done

---
+++ app/src/main/java/com/ironclad/wedigittest/domain/ApiRepository.kt
@@ -0,0 +1,16 @@
+package com.ironclad.wedigittest.domain
+
+import com.ironclad.wedigittest.data.api.ApiHelper
+import javax.inject.Inject
+
+class ApiRepository @Inject constructor(
+    private val apiHelper: ApiHelper
+) {
+    suspend fun getPopularMovies(queries: Map<String, Any>) = apiHelper.getPopularMovies(queries)
+    suspend fun getMovieDetails(movieId: Int, queries: Map<String, Any>) =
+        apiHelper.getMovieDetails(movieId, queries)
+    suspend fun getCreditDetails(
+        movieId: Int,
+        queries: Map<String, Any>
+    ) = apiHelper.getCreditDetails(movieId, queries)
+}
